143ade7c5438101831b7b9b8adc16347f6b7adea
Christophe CHICHMANIAN <chris.chich@hotmail.fr>
2023-12-30 20:31:45 +0100

    :sparkles: Add angular material

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,7 +6,8 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getStorage, provideStorage } from '@angular/fire/storage';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"my-progress-da34b","appId":"1:563359593576:web:3412d228e749a9219bb7dd","storageBucket":"my-progress-da34b.appspot.com","locationId":"europe-west","apiKey":"AIzaSyDZL_Jg4LQQCYhtcjbUsoJnAvGRLvgRZ3Q","authDomain":"my-progress-da34b.firebaseapp.com","messagingSenderId":"563359593576"}))), importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideFirestore(() => getFirestore())), importProvidersFrom(provideStorage(() => getStorage()))]
+  providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({ "projectId": "my-progress-da34b", "appId": "1:563359593576:web:3412d228e749a9219bb7dd", "storageBucket": "my-progress-da34b.appspot.com", "locationId": "europe-west", "apiKey": "AIzaSyDZL_Jg4LQQCYhtcjbUsoJnAvGRLvgRZ3Q", "authDomain": "my-progress-da34b.firebaseapp.com", "messagingSenderId": "563359593576" }))), importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideFirestore(() => getFirestore())), importProvidersFrom(provideStorage(() => getStorage())), provideAnimations()]
 };
