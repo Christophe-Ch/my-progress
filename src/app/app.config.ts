0ad9628eb70ef384426d1a4ba95efcac4112e842
Christophe CHICHMANIAN <chris.chich@hotmail.fr>
2023-12-30 20:30:26 +0100

    :sparkles: Add angular fire

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,12 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"my-progress-da34b","appId":"1:563359593576:web:3412d228e749a9219bb7dd","storageBucket":"my-progress-da34b.appspot.com","locationId":"europe-west","apiKey":"AIzaSyDZL_Jg4LQQCYhtcjbUsoJnAvGRLvgRZ3Q","authDomain":"my-progress-da34b.firebaseapp.com","messagingSenderId":"563359593576"}))), importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideFirestore(() => getFirestore())), importProvidersFrom(provideStorage(() => getStorage()))]
 };
