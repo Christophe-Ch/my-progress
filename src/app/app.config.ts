a3607648423a158138992e1bf346bab8e76a1d75
Christophe CHICHMANIAN <chris.chich@hotmail.fr>
2023-12-30 20:43:50 +0100

    :wrench: Add environment configurations and setup angular fire

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,7 +7,32 @@ import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getStorage, provideStorage } from '@angular/fire/storage';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { environment } from '../environments/environment';
+import { USE_EMULATOR as USE_AUTH_EMULATOR } from '@angular/fire/compat/auth';
+import { USE_EMULATOR as USE_FIRESTORE_EMULATOR } from '@angular/fire/compat/firestore';
+import { USE_EMULATOR as USE_FUNCTIONS_EMULATOR } from '@angular/fire/compat/functions';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({ "projectId": "my-progress-da34b", "appId": "1:563359593576:web:3412d228e749a9219bb7dd", "storageBucket": "my-progress-da34b.appspot.com", "locationId": "europe-west", "apiKey": "AIzaSyDZL_Jg4LQQCYhtcjbUsoJnAvGRLvgRZ3Q", "authDomain": "my-progress-da34b.firebaseapp.com", "messagingSenderId": "563359593576" }))), importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideFirestore(() => getFirestore())), importProvidersFrom(provideStorage(() => getStorage())), provideAnimations()]
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(
+      provideFirebaseApp(() => initializeApp(environment.firebase))
+    ),
+    importProvidersFrom(provideAuth(() => getAuth())),
+    importProvidersFrom(provideFirestore(() => getFirestore())),
+    importProvidersFrom(provideStorage(() => getStorage())),
+    provideAnimations(),
+    {
+      provide: USE_AUTH_EMULATOR,
+      useValue: !environment.production ? ['localhost', 9099] : undefined,
+    },
+    {
+      provide: USE_FIRESTORE_EMULATOR,
+      useValue: !environment.production ? ['localhost', 8080] : undefined,
+    },
+    {
+      provide: USE_FUNCTIONS_EMULATOR,
+      useValue: !environment.production ? ['localhost', 5001] : undefined,
+    },
+  ],
 };
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'my-progress-da34b',
+    appId: '1:563359593576:web:3412d228e749a9219bb7dd',
+    storageBucket: 'my-progress-da34b.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyDZL_Jg4LQQCYhtcjbUsoJnAvGRLvgRZ3Q',
+    authDomain: 'my-progress-da34b.firebaseapp.com',
+    messagingSenderId: '563359593576',
+  },
+  production: true,
+};
